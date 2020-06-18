e5dd8f095fdf2bbbad48a107b9d9d7152ab0fb6b
Dongaben <gaborlevel@freemail.hu>
2020-06-18 17:54:57 +0200

    Init page.

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function login() {
+    
+    let loginInput = document.querySelector("input[name='login-input']");
+    
+    if (loginInput.value == "Szabó Krisztina Dóra") {
+        alert("Szia Titti!");
+        ablak = open("Titti.html")
+        
+    } 
+   else if (loginInput.value != "Szabó Krisztina Dóra") {
+        alert("Nem ismerlek, viszlát!");
+    }
+}
+
+function bezárás() {
+    ablak = close("Titti.html");
+}
